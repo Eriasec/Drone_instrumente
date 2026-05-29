a17a653947fd52c193f3a8cb5c61e4f0b68f7b95
Eriasec <124135956+Eriasec@users.noreply.github.com>
2026-05-29 11:55:48 +0200

    V2.2

--- Drone instrumente S6/src/main.cpp
+++ Drone instrumente S6/src/main.cpp
@@ -1,21 +1,40 @@
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-#include <Wire.h>
-#include "Adafruit_SGP30.h"
-#include <Adafruit_BMP280.h>
-#include <BluetoothSerial.h>
 
-#define DHTPIN 33       // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT12   // DHT 12
+// Parametres
+#define ENABLE_DEBUG
+#define ENABLE_DHT12
+#define ENABLE_SGP30
+//#define ENABLE_BMP280
+#define ENABLE_BLUETOOTH
+
+#ifdef ENABLE_DHT12
+  #include <Wire.h>
+  #include <DHT.h>
+  #include <DHT_U.h>
+  #define DHTPIN 33       // Digital pin connected to the DHT sensor
+  #define DHTTYPE DHT12   // DHT 12
+  DHT dht(DHTPIN, DHTTYPE); // Init DHT sensor
+#endif
+
+#ifdef ENABLE_SGP30
+  #include <Wire.h>
+  #include <Adafruit_SGP30.h>
+  Adafruit_SGP30 sgp;
+#endif
+
+#ifdef ENABLE_BMP280
+  #include <Wire.h>
+  #include <Adafruit_BMP280.h>
+  Adafruit_BMP280 bmp; // I2C
+#endif
+
+#ifdef ENABLE_BLUETOOTH
+  #include "BluetoothSerial.h"
+  BluetoothSerial SerialBT;
+#endif
 
 uint32_t getAbsoluteHumidity(float temperature, float humidity);
-
-DHT dht(DHTPIN, DHTTYPE); // Init DHT sensor
-Adafruit_SGP30 sgp;
-// Adafruit_BMP280 bmp; // I2C
-BluetoothSerial SerialBT;
 
 float h, t; // Variables to hold humidity and temperature values
 float hic;  // Variable to hold heat index value
@@ -23,93 +42,145 @@ int counter = 0;
 const char* pin = "1234"; // Bluetooth pairing pin
 
 void setup() {
-  Serial.begin(115200); // Init serial
-  Serial.println("Init");
-  
-  // _____ Init Temperature/Humidity sensor _____ //
-  dht.begin(); // Start DHT sensor
+  #ifdef ENABLE_DEBUG     // Init Serial for debugging
+    Serial.begin(115200);
+    Serial.println("Init");
+  #endif
 
-  // _____ Init Air Quality sensor _____ //
-  if (! sgp.begin()){
-    Serial.println("Sensor not found :(");
-    while (1) delay(10);
-  }
-  Serial.print("Found SGP30 serial #");
-  Serial.print(sgp.serialnumber[0], HEX);
-  Serial.print(sgp.serialnumber[1], HEX);
-  Serial.println(sgp.serialnumber[2], HEX);
+  #ifdef ENABLE_DHT12     //  Init Temperature/Humidity sensor
+    dht.begin();
+  #endif
 
-  // // _____ Init Pressure sensor _____ //
-  // if (!bmp.begin()) {
-  //   Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-  //                     "try a different address!"));
-  //   while (1) delay(10);
-  // }
+  #ifdef ENABLE_SGP30     // Init Air Quality sensor
+    if (! sgp.begin()){
+      #ifdef ENABLE_DEBUG
+        Serial.println("Sensor not found :(");
+      #endif
+      while (1) delay(10);
+    }
+    #ifdef ENABLE_DEBUG
+      Serial.print("Found SGP30 serial #");
+      Serial.print(sgp.serialnumber[0], HEX);
+      Serial.print(sgp.serialnumber[1], HEX);
+      Serial.println(sgp.serialnumber[2], HEX);
+    #endif
+  #endif
 
-  // _____ Init Bluetooth _____ //
-  SerialBT.setPin(pin);                   // Set Bluetooth pairing pin
-  SerialBT.begin("ESP32_METEO_STATION");  // Bluetooth device name
+  #ifdef ENABLE_BMP280    //Init Pressure sensor
+    if (!bmp.begin()) {
+      #ifdef ENABLE_DEBUG
+        Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                        "try a different address!"));
+      #endif
+      while (1) delay(10);
+    }
+  #endif
+
+  #ifdef ENABLE_BLUETOOTH // Init Bluetooth
+    SerialBT.begin("ESP32_METEO_STATION");  // Bluetooth device name
+  #endif
 }
 
 void loop() {
   delay(2000); // Wait a few seconds between measurements. 
-  h = dht.readHumidity();
-  t = dht.readTemperature();
+  #ifdef ENABLE_DHT12     // DHT12 readings
+    h = dht.readHumidity();
+    t = dht.readTemperature();
 
-  if(isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.println();
-
-  SerialBT.print(F("H"));
-  SerialBT.print(h);
-  SerialBT.print(F("T"));
-  SerialBT.print(t);
-  SerialBT.print("\n");
-
-  // Compute heat index in Celsius (isFahreheit = false)
-  hic = dht.computeHeatIndex(t, h, false);
-
-  // If you have a temperature / humidity sensor, you can set the absolute humidity to enable the humditiy compensation for the air quality signals
-  sgp.setHumidity(getAbsoluteHumidity(t, h));
-
-  if (! sgp.IAQmeasure()) {
-    Serial.println("Measurement failed");
-    return;
-  }
-  Serial.print("TVOC "); Serial.print(sgp.TVOC); Serial.print(" ppb\t");
-  Serial.print("eCO2 "); Serial.print(sgp.eCO2); Serial.println(" ppm");
-
-  if (! sgp.IAQmeasureRaw()) {
-    Serial.println("Raw Measurement failed");
-    return;
-  }
-  Serial.print("Raw H2 "); Serial.print(sgp.rawH2); Serial.print(" \t");
-  Serial.print("Raw Ethanol "); Serial.print(sgp.rawEthanol); Serial.println("");
-
-  delay(1000);
-
-  counter++;
-  if (counter == 30) {
-    counter = 0;
-
-    uint16_t TVOC_base, eCO2_base;
-    if (! sgp.getIAQBaseline(&eCO2_base, &TVOC_base)) {
-      Serial.println("Failed to get baseline readings");
+    if(isnan(h) || isnan(t)) {
+      #ifdef ENABLE_DEBUG
+        Serial.println("Failed to read from DHT sensor!");
+      #endif
       return;
     }
-    Serial.print("****Baseline values: eCO2: 0x"); Serial.print(eCO2_base, HEX);
-    Serial.print(" & TVOC: 0x"); Serial.println(TVOC_base, HEX);
-  }
+
+    // Compute heat index in Celsius (isFahreheit = false)
+    hic = dht.computeHeatIndex(t, h, false);
+
+    #ifdef ENABLE_DEBUG
+      Serial.print(F("Humidity: "));
+      Serial.print(h);
+      Serial.print(F("%  Temperature: "));
+      Serial.print(t);
+      Serial.print(F("°C  Heat index: "));
+      Serial.print(hic);
+      Serial.print(F("°C "));
+      Serial.println();
+    #endif
+
+    #ifdef ENABLE_BLUETOOTH
+      SerialBT.print(F("HUM"));
+      SerialBT.print(h);
+      SerialBT.print(F(" TEMP"));
+      SerialBT.print(t);
+      SerialBT.print(F(" INDEX"));
+      SerialBT.print(hic);
+      #ifndef ENABLE_SGP30
+        #ifndef ENABLE_BMP280
+          SerialBT.print(F("\n"));
+        #endif
+      #endif
+    #endif
+    
+    #ifdef ENABLE_SGP30 // Set absolute humidity for SGP30 compensation
+      sgp.setHumidity(getAbsoluteHumidity(t, h));
+    #endif
+  #endif
+
+  #ifdef ENABLE_SGP30     // SGP30 readings
+    if (! sgp.IAQmeasure()) {
+      #ifdef ENABLE_DEBUG
+        Serial.println("Measurement failed");
+      #endif
+      return;
+    }
+    #ifdef ENABLE_DEBUG
+      Serial.print("TVOC "); Serial.print(sgp.TVOC); Serial.print(" ppb\t");
+      Serial.print("eCO2 "); Serial.print(sgp.eCO2); Serial.println(" ppm");
+    #endif
+
+    if (! sgp.IAQmeasureRaw()) {
+      #ifdef ENABLE_DEBUG
+        Serial.println("Raw Measurement failed");
+      #endif
+      return;
+    }
+    #ifdef ENABLE_DEBUG
+      Serial.print("Raw H2 "); Serial.print(sgp.rawH2); Serial.print(" \t");
+      Serial.print("Raw Ethanol "); Serial.print(sgp.rawEthanol); Serial.println("");
+    #endif
+
+    delay(1000);
+
+    counter++;
+    if (counter == 30) {
+      counter = 0;
+
+      uint16_t TVOC_base, eCO2_base;
+      if (! sgp.getIAQBaseline(&eCO2_base, &TVOC_base)) {
+        #ifdef ENABLE_DEBUG
+          Serial.println("Failed to get baseline readings");
+        #endif
+        return;
+      }
+      #ifdef ENABLE_DEBUG
+        Serial.print("****Baseline values: eCO2: 0x"); Serial.print(eCO2_base, HEX);
+        Serial.print(" & TVOC: 0x"); Serial.println(TVOC_base, HEX);
+      #endif
+    }
+
+    #ifdef ENABLE_BLUETOOTH
+      SerialBT.print(F(" TVOC"));
+      SerialBT.print(sgp.TVOC);
+      SerialBT.print(F(" eCO2"));
+      SerialBT.print(sgp.eCO2);
+      SerialBT.print(F(" RAWH2"));
+      SerialBT.print(sgp.rawH2);
+      SerialBT.print(F(" RAWEthanol"));
+      SerialBT.print(sgp.rawEthanol);
+      SerialBT.print(F("\n"));
+      #endif
+  #endif
 }
 
 
